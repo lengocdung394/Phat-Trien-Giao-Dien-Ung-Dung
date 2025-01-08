bab19ebaab2694b71ff9431cf8e6c4fe397b6f74
Co Phuong Hang <Dungheongunhubo>
2025-01-08 10:03:12 +0700

    finish Coding Challenge #1

---
+++ script.js
@@ -0,0 +1,14 @@
+// Coding Challenge #1
+var massM = 78
+var heightM = 1.69
+
+var massJ = 95
+var heightJ = 1.88
+
+var BMIM = massM / heightM ** 2
+var BMIJ = massJ / heightJ ** 2
+
+var higherBMI = BMIM > BMIJ
+
+console.log(BMIM, BMIJ, higherBMI)
+
